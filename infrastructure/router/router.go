3f4c5281f6dcba6094453e6ade4b4cb5dcbf03cd
Hansel Mendoza <hansel.mendoza@boats.com>
2021-11-19 17:18:05 -0600

    FIRST-DELIVERABLE: adding logger to the router

--- infrastructure/router/router.go
+++ infrastructure/router/router.go
@@ -1,11 +1,21 @@
 package router
 
 import (
-	"github.com/hamg26/academy-go-q42021/interface/controllers"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+
+	"github.com/hamg26/academy-go-q42021/config"
+	"github.com/hamg26/academy-go-q42021/interface/controllers"
 )
 
 func NewRouter(e *echo.Echo, c controller.AppController) *echo.Echo {
+	config.ReadConfig()
+
+	if config.C.Logging == true {
+		e.Use(middleware.Logger())
+	}
+	e.Use(middleware.Recover())
+
 	e.GET("/pokemons", func(context echo.Context) error { return c.GetPokemons(context) })
 	e.GET("/pokemons/:id", func(context echo.Context) error { return c.GetPokemon(context) })
 	return e
