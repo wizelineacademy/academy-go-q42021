8cf4cab88fa0ee9a2fe8e55a1588e5d3e100ad1a
JuanLuis Lopez <juanluis.lopez@wizeline.com>
2021-12-07 21:56:58 -0800

    set routes and handlers on Router.go

--- infraestructure/router/router.go
+++ infraestructure/router/router.go
@@ -1,6 +1,25 @@
 package router
 
-var SearchPokemon = "/buscarPokemon"
-var ListPokemon = "/listarPokemon"
-var ListPokemonConcurrently = "/listarPokemonConcurrently"
-var Port = ":8081"
+import (
+	controller "mainRoot/interface/controller"
+	"net/http"
+)
+
+const SearchPokemon = "/buscarPokemon"
+const ListPokemon = "/listarPokemon"
+const ListPokemonConcurrently = "/listarPokemonConcurrently"
+const Port = ":8081"
+
+func Listen() {
+	http.HandleFunc(ListPokemon, func(w http.ResponseWriter, peticion *http.Request) {
+		controller.ListarPokemones(w, peticion)
+	})
+	http.HandleFunc(SearchPokemon, func(w http.ResponseWriter, peticion *http.Request) {
+		controller.BuscarPokemones(w, peticion)
+	})
+
+	http.HandleFunc(ListPokemonConcurrently, func(w http.ResponseWriter, peticion *http.Request) {
+		controller.ListarPokemonesConcurrently(w, peticion)
+	})
+
+}
--- main.go
+++ main.go
@@ -5,22 +5,12 @@ import (
 	"log"
 	router "mainRoot/infraestructure/router"
 	controller "mainRoot/interface/controller"
-	"net/http"
+	http "net/http"
 )
 
 func main() {
 	controller.LlenarPokedex()
-
-	http.HandleFunc(router.ListPokemon, func(w http.ResponseWriter, peticion *http.Request) {
-		controller.ListarPokemones(w, peticion)
-	})
-	http.HandleFunc(router.SearchPokemon, func(w http.ResponseWriter, peticion *http.Request) {
-		controller.BuscarPokemones(w, peticion)
-	})
-
-	http.HandleFunc(router.ListPokemonConcurrently, func(w http.ResponseWriter, peticion *http.Request) {
-		controller.ListarPokemonesConcurrently(w, peticion)
-	})
+	router.Listen()
 
 	fmt.Println("Servidor listo escuchando en " + router.Port)
 	log.Fatal(http.ListenAndServe(router.Port, nil))
