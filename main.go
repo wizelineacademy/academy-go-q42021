f99636f594661e7d42eb50ef029a01655691f55a
JuanLuis Lopez <juanluis.lopez@wizeline.com>
2021-12-08 12:05:16 -0800

    c

--- main.go
+++ main.go
@@ -5,13 +5,22 @@ import (
 	"log"
 	router "mainRoot/infraestructure/router"
 	controller "mainRoot/interface/controller"
-	http "net/http"
+	"net/http"
 )
 
-//
 func main() {
 	controller.LlenarPokedex()
-	router.Listen()
+
+	http.HandleFunc(router.ListPokemon, func(w http.ResponseWriter, peticion *http.Request) {
+		controller.ListarPokemones(w, peticion)
+	})
+	http.HandleFunc(router.SearchPokemon, func(w http.ResponseWriter, peticion *http.Request) {
+		controller.BuscarPokemones(w, peticion)
+	})
+
+	http.HandleFunc(router.ListPokemonConcurrently, func(w http.ResponseWriter, peticion *http.Request) {
+		controller.ListarPokemonesConcurrently(w, peticion)
+	})
 
 	fmt.Println("Servidor listo escuchando en " + router.Port)
 	log.Fatal(http.ListenAndServe(router.Port, nil))
